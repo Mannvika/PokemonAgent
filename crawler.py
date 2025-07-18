9bcf005656319cea6aa5fe202ea21c12fa2bb5c0
Mannvika <mannyjainlakes@gmail.com>
2025-07-18 21:25:14 +0900

    Developed crawler script to get title and lore_text from bulbapedia towns and cities

--- crawler.py
+++ crawler.py
@@ -1,22 +1,31 @@
 from pathlib import Path
-import scrapy as sc
+import scrapy
 
-class PokemonCrawler(sc.Spider):
+class PokemonCrawler(scrapy.Spider):
     name = 'pokemon'
 
-    async def start(self):
-        self.start_urls = [
-            'https://bulbapedia.bulbagarden.net/wiki/List_of_Pok%C3%A9mon_by_National_Pok%C3%A9dex_number',
-            'https://bulbapedia.bulbagarden.net/wiki/List_of_cities_and_townsr',
-            'https://bulbapedia.bulbagarden.net/wiki/List_of_cross-canon_references'
-            'https://bulbapedia.bulbagarden.net/wiki/List_of_cross-generational_references'
-        ]
+    start_urls = [
+        'https://bulbapedia.bulbagarden.net/wiki/List_of_cities_and_towns',
+    ]
         
-        for url in urls:
-            yield sc.Request(url=url, callback=self.parse)
-
     def parse(self, response):
-        page = response.url.split("/")[-2]
-        filename = f"pokemons-{page}.html"
-        Path('data').mkdir(parents=True, exist_ok=True)
-        self.log(f'Saving page {page} to {filename}')
+        print(f"Parsing URL: {response.url}")
+        for url in response.css('table.sortable td:first-child a::attr(href)').getall():
+            print(f"Found URL: {url}")
+            if url.startswith('/wiki/') and not url.startswith('/wiki/File:'):
+                print(f"Following URL: {url}")
+                yield response.follow(url, self.parse_lore)
+    
+    def parse_lore(self, response):
+        title = response.css("span.mw-page-title-main::text").get()
+        
+        lore_paragraphs = response.xpath('//h2[1]/preceding-sibling::p').css('::text').getall()
+        
+        lore_text = ' '.join(p.strip() for p in lore_paragraphs).strip()
+        
+        yield {
+            'title': title,
+            'lore': lore_text,
+        }
+
+
