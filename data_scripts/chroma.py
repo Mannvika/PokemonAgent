6542492b85bda116c85c8173871cb2b211795a26
Mannvika <mannyjainlakes@gmail.com>
2025-08-02 21:38:24 -0400

    Added trainer info to SQLite and Chroma DB

--- data_scripts/chroma.py
+++ data_scripts/chroma.py
@@ -1,10 +1,13 @@
 import chromadb
 import sqlite3
 
-with sqlite3.connect('../pokemon.db') as connection:
+with sqlite3.connect('./pokemon.db') as connection:
     cursor = connection.cursor()
     cursor.execute('SELECT name, lore FROM towns')
     towns = cursor.fetchall()
+
+    cursor.execute('SELECT name, lore FROM trainers')
+    trainers = cursor.fetchall()
 
 client = chromadb.PersistentClient(path="chroma_db")
 collection = client.get_or_create_collection(name="pokemon_towns")
@@ -12,6 +15,11 @@ collection = client.get_or_create_collection(name="pokemon_towns")
 ids = [town[0] for town in towns]
 documents = [town[1] for town in towns]
 metdatas = [{"name": town[0]} for town in towns]
+
+for trainer in trainers:
+    ids.append(trainer[0])
+    documents.append(trainer[1])
+    metdatas.append({"name": trainer[0], "type": "trainer"})
 
 collection.add(
     documents=documents,
--- data_scripts/sqlite.py
+++ data_scripts/sqlite.py
@@ -1,20 +1,23 @@
 import sqlite3
 import json
 
-connection = sqlite3.connect('../pokemon.db')
+connection = sqlite3.connect('./pokemon.db')
 
 cursor = connection.cursor()
 
 # sql = 'create table if not exists towns (name text primary key, lore text)'
+sql = 'drop table if exists trainers'
+cursor.execute(sql)
 sql = 'create table if not exists trainers (name text primary key, lore text)'
+cursor.execute(sql)
 
-with open('lore.json', 'r') as file:
+with open('trainers.json', 'r') as file:
     lore_data = json.load(file)
 
 for entry in lore_data:
     name = entry['title']
     lore = entry['lore']
-    sql = 'insert or ignore into towns (name, lore) values (?, ?)'
+    sql = 'insert or ignore into trainers (name, lore) values (?, ?)'
     cursor.execute(sql, (name, lore))
 
 #cursor.execute(sql)
